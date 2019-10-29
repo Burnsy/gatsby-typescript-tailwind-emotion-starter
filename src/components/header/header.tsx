ec24b4d7b26fe37119d5abb5df8998846322f2bf
Christopher Burns <christopher@tapkit.io>
2019-10-29 08:15:57 +0000

    working typescript + tailwind css

---
+++ src/components/header/header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container, StyledLink, Title } from './header.css';
+
+
+interface HeaderProps {
+  siteTitle:string
+}
+
+const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
+  <Container>
+    <StyledLink to="/">
+      <Title>{siteTitle}</Title>
+    </StyledLink>
+  </Container>
+)
+
+export default Header
---
+++ src/components/layout.tsx
@@ -0,0 +1,43 @@
+import './layout.css';
+
+import styled from '@emotion/styled';
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+import Helmet from 'react-helmet';
+import tw from 'tailwind.macro';
+
+import Header from './header/header';
+
+const Content = styled.div(tw`px-32 py-8 font-sans`);
+
+const Layout: React.FC<{children: React.ReactNode}> = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <Content>{children}</Content>
+      </>
+    )}
+  />
+)
+
+
+export default Layout
